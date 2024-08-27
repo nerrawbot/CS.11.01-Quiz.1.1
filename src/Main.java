948167f3abe62ca27ca12548e74dff8fbcbc7e67
chengqifengwarren <chengqifeng2009@gmail.com>
2024-08-27 14:08:15 +0800

    IDE formative

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
+        System.out.println("Enter your age: ");
+        age = scanner.nextInt();
+        System.out.println("Enter your first name: ");
+        firstName = scanner.next();
+        System.out.println("Enter your favourite food: ");
+        favouriteFood = scanner.next();
+        System.out.println("First name: " + firstName + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
     }
 
 }
